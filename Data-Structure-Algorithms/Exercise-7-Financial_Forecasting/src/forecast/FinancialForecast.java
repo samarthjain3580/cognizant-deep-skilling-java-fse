8495b679d4e7004f8e310be8233e9d470fc6620a
Samarth Jain <samarthjain3580@gmail.com>
2026-06-28 15:32:58 +0530

    Complete DSA Exercise 7 - Financial Forecasting

---
+++ Data-Structure-Algorithms/Exercise-7-Financial_Forecasting/src/forecast/FinancialForecast.java
@@ -0,0 +1,42 @@
+package forecast;
+
+public class FinancialForecast {
+
+    public static double forecast(double amount,
+                                  double rate,
+                                  int years) {
+
+        if (years == 0)
+            return amount;
+
+        return forecast(amount * (1 + rate),
+                        rate,
+                        years - 1);
+    }
+
+    public static void main(String[] args) {
+
+        double currentValue = 10000;
+
+        double growthRate = 0.10;
+
+        int years = 5;
+
+        double futureValue =
+                forecast(currentValue,
+                         growthRate,
+                         years);
+
+        System.out.println("Current Value : ₹" + currentValue);
+
+        System.out.println("Growth Rate : "
+                + (growthRate * 100) + "%");
+
+        System.out.println("Years : " + years);
+
+        System.out.printf("Future Value : ₹%.2f%n",
+                futureValue);
+
+    }
+
+}
