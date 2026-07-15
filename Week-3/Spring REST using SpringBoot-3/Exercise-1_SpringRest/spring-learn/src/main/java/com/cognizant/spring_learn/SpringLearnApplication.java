59984ff71ee211c43342df586fc3a6025419d2c0
Samarth Jain <samarthjain3580@gmail.com>
2026-07-15 15:35:43 +0530

    Complete exercise 1 of week 3

--- Week-3/Spring REST using SpringBoot-3/Exercise-1_SpringRest/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
+++ Week-3/Spring REST using SpringBoot-3/Exercise-1_SpringRest/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -2,12 +2,31 @@ package com.cognizant.spring_learn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.cognizant.spring_learn.model.Country;
 
 @SpringBootApplication
 public class SpringLearnApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringLearnApplication.class, args);
-	}
+    public static void main(String[] args) {
 
+        SpringApplication.run(SpringLearnApplication.class, args);
+
+        displayCountry();
+    }
+
+    public static void displayCountry() {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        Country country =
+                context.getBean("country", Country.class);
+
+        System.out.println(country);
+
+        ((ClassPathXmlApplicationContext) context).close();
+    }
 }
