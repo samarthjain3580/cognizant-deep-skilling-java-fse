5421c678570ed433d4705fdbf3b7628cc9aac398
Samarth Jain <samarthjain3580@gmail.com>
2026-07-16 14:34:58 +0530

    Week 3: Completed Exercise 1 - Hello World RESTful Web Service

---
+++ Week-3/Spring REST using SpringBoot-3/Exercise-2_SpringRest/spring-rest-handson/src/main/java/com/cognizant/spring_rest_handson/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.cognizant.spring_rest_handson.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+
+        System.out.println("START");
+
+        String message = "Hello World!!";
+
+        System.out.println("END");
+
+        return message;
+    }
+
+}
